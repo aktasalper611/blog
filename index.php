8702c6affe226de87185e7c850d7734299101fe7
aktasalper611 <43081548+aktasalper611@users.noreply.github.com>
2018-09-08 04:06:33 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>My Blog</title>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+	<link href="https://fonts.googleapis.com/css?family=Indie+Flower|Kalam" rel="stylesheet">
+    <meta name=”author” content="Alper Aktaş" />
+    <meta name=”description” content="Web sitemden hakkımda blgi edinebilirsiniz." />
+	<link rel="stylesheet" type="text/css" href="css.css">
+</head>
+<body>
+
+
+	<div class="arkaplan" >
+		<audio controls autoplay="autoplay" style="display:none" >
+			
+			<source src="barısmanco.mp3" type="audio/mpeg">
+			Tarayıcınız audio elementini desteklemiyor.
+		</audio>
+
+		<div class="sol"  >
+
+			<div class="akis" id="akis">
+				<div class="foto">
+
+				</div>
+				<p> Alper Aktaş </p>
+				<center><small >Burasıda Benim Mağaram :) </small>
+				</center>
+			</div>
+
+		</div>
+
+
+
+		<div class="sag" id="sag" >
+			<h1>Kariyer Hedefi</h1>
+			<p>Her gün yazılım ve teknoloji giderek gelişiyor.Amacım bu sektörde bilgilerimi geliştirerek biraz olsun ülkede farkındalık oluşturabilmek.Bunun için gerek oluşturmaya çalıştığımız şirket gerek bu sektöre meraklı kişileri bir araya getirmeye çalışmaktayız. <span style="font-size: 20px">"Fırtınalara yön veren kelebeklerin kanat çırpışıdır."</span></p>
+			<hr style="background-color:white">
+			<span style="font-size: 20px">
+				Telefon: 05386430269 <br>
+				E-Posta: aktasalper61@gmail.com <br>
+				Web Sitesi: Çok yakında..
+
+			</span>
+			<br> <br> <br>
+
+		</div>
+		
+	</div>
+
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+<script type="text/javascript" src="aaa.js"></script>
+
+<script type="text/javascript">$(document).ready(function(){
+	$("#akis").slideToggle(1000);
+});
+</script>
+<script type="text/javascript">$(document).ready(function(){
+	$("#sag").slideToggle(2500);
+});
+</script>
+</body>
+</html>
